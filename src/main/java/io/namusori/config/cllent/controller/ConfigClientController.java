baac5a698cb70700a327680fe74f50141de3a7db
Lee Cheol <edslclee@gmail.com>
2022-01-28 09:26:05 +0900

    init

---
+++ src/main/java/io/namusori/config/cllent/controller/ConfigClientController.java
@@ -0,0 +1,17 @@
+package io.namusori.config.cllent.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${test.str}")
+    private String configStr;
+
+    @GetMapping("/test")
+    public String test() {
+        return configStr;
+    }
+}
